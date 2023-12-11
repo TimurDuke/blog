4fc4380f0d1cf76e1487a14d1b46ab7766a4a8c8
TimurDuke <primecsgo2018@gmail.com>
2023-12-12 01:07:55 +0900

    created login/register pages and set up routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/configureStore";
+import { Login, Register } from './containers/Authorization';
+import Articles from "./containers/Articles";
+import ArticlesItem from "./components/ArticlesItem";
 
-const App = () => {
-    return (
-        <div>
-          Hi
-        </div>
-    );
-};
+const App = () => (
+    <Provider store={store}>
+        <Routes>
+            <Route path="/" element={<Navigate to="/articles" replace />}/>
+            <Route path="/articles" element={<Articles/>} />
+            <Route path="/articles/:slug" element={<ArticlesItem/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/register" element={<Register/>} />
+        </Routes>
+    </Provider>
+);
 
 export default App;
---
+++ src/containers/Authorization/Register/Register.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Register = () => {
+    console.log('register')
+    return (
+        <div>
+            Register page
+        </div>
+    );
+};
+
+export default Register;
