85d5d98241a702a9390bbdaf5427928f4d4f390a
TimurDuke <primecsgo2018@gmail.com>
2023-12-22 01:18:28 +0900

    added create article component

--- src/containers/Articles/Articles.js
+++ src/containers/Articles/Articles.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Pagination} from "@mui/material";
 import {useDispatch, useSelector} from "react-redux";
+import {useLocation} from "react-router-dom";
 import {useGetAllArticlesQuery} from "../../services/ArticlesService";
 import ArticleCard from "../../components/ArticleCard";
 import {formattedDate} from "../../utils/articleUtils";
 import {setPage} from "../../store/actions/articlesActions";
+import {loginPath, logoutPath, registerPath} from "../../routes/routePaths";
 
 const Articles = () => {
     const dispatch = useDispatch();
+    const location = useLocation();
 
     const { page } = useSelector(state => state.articles);
+    const { user } = useSelector(state => state.user);
 
-    const {data, isFetching} = useGetAllArticlesQuery(page);
+    const authPaths = [registerPath, loginPath, logoutPath];
+    const fromPath = location.state?.from;
+
+    const { data, isFetching, refetch} = useGetAllArticlesQuery(page);
+
+    useEffect(() => {
+        if (authPaths.includes(fromPath)) {
+            refetch();
+        }
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [fromPath, refetch, user]);
 
     if (isFetching) {
         return <h2>Loading...</h2>
@@ -23,7 +38,7 @@ const Articles = () => {
 
     return (
         <>
-            {data.articles && data.articles.map(article => (
+            {data?.articles && data?.articles.map(article => (
                 <ArticleCard
                     key={article.slug}
                     slug={article.slug}
@@ -35,10 +50,10 @@ const Articles = () => {
                     date={formattedDate(article['createdAt'])}
                 />
             ))}
-            {data['articlesCount'] &&
+            {data?.articlesCount &&
                 <Pagination
                     style={{display: 'flex', justifyContent: 'center'}}
-                    count={data['articlesCount']}
+                    count={data.articlesCount}
                     shape="rounded"
                     color="primary"
                     onChange={paginationHandler}
---
+++ src/containers/CreateArticle/CreateArticle.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {useNavigate} from "react-router-dom";
+import ArticleForm from "../../components/UI/Form/ArticleForm";
+import {fieldArticleConfig} from "../../utils/inputRuleUtils";
+import {useCreateArticleMutation} from "../../services/ArticlesService";
+import {articlesPath} from "../../routes/routePaths";
+
+const CreateArticle = () => {
+    const navigate = useNavigate();
+
+    const [createArticle, {isLoading}] = useCreateArticleMutation();
+
+    const submitHandler = async data => {
+        const tagsArray = data.tags
+            .map(tagObj => tagObj.tag)
+            .filter(tag => tag.trim() !== '')
+
+        const article = {
+            ...data,
+            tags: tagsArray
+        };
+
+        try {
+            await createArticle({article}).unwrap();
+            navigate(articlesPath);
+        } catch (e) {
+            // eslint-disable-next-line no-console
+            console.error(e);
+        }
+    };
+
+    return (
+        <>
+            <ArticleForm
+                fieldConfig={fieldArticleConfig}
+                submitHandler={submitHandler}
+                isLoading={isLoading}
+            />
+        </>
+    );
+};
+
+export default CreateArticle;
