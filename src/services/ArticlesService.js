739cb8293815fe71859b2e1ddba6e4247c1f3e77
TimurDuke <primecsgo2018@gmail.com>
2023-12-22 01:15:59 +0900

    added article creation request

--- src/services/ArticlesService.js
+++ src/services/ArticlesService.js
@@ -1,6 +1,7 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import {apiUrl} from "../config";
 import {ARTICLES_LIMIT_COUNT} from "../components/constants";
+import {getHeaders} from "../utils/headerUtils";
 
 export const articlesAPI = createApi({
     reducerPath: 'articlesAPI',
@@ -9,22 +10,33 @@ export const articlesAPI = createApi({
     endpoints: (build) => ({
         getAllArticles: build.query({
             query: (page = 1) => ({
-                url: `articles?limit=${ARTICLES_LIMIT_COUNT}&offset=${page}`
+                url: `articles?limit=${ARTICLES_LIMIT_COUNT}&offset=${page}`,
+                headers: getHeaders()
             }),
-            providesTags: (result) =>
-                result.articles
-                    ? [
-                        ...result.articles.map(({slug}) => ({type: 'Articles', slug})),
-                        { type: 'Articles', slug: 'LIST' },
-                      ]
-                    : [{ type: 'Articles', slug: 'LIST' }],
+            keepUnusedDataFor: 5,
+            providesTags: (result) => result.articles
+                ? [
+                    ...result.articles.map(({slug}) => ({type: 'Articles', slug})),
+                    {type: 'Articles', slug: 'LIST'},
+                ]
+                : [{type: 'Articles', slug: 'LIST'}],
         }),
         getArticle: build.query({
             query: (slug) => ({
-                url: `articles/${slug}`
+                url: `articles/${slug}`,
+                headers: getHeaders()
             }),
+        }),
+        createArticle: build.mutation({
+            query: (body) => ({
+                url: 'articles',
+                method: 'POST',
+                body,
+                headers: getHeaders()
+            }),
+            invalidatesTags: [{type: 'Articles', slug: 'LIST'}],
         }),
     }),
 });
 
-export const {useGetAllArticlesQuery, useGetArticleQuery} = articlesAPI;
+export const {useGetAllArticlesQuery, useGetArticleQuery, useCreateArticleMutation} = articlesAPI;
